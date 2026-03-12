806b6bfc698839cd888ee17b5f93d304271c002c
Cat_McBrain <ajersa05@gmail.com>
2026-03-12 10:27:59 +0100

    vault backup: 12-03-2026 10:27:59

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -3,13 +3,18 @@
 int main()
 {
 
-	int age=0;
-	float gpa=0;
-	char grade='\0';
-	char name[30]="";
+	char item[50] = "";
+	float price = 0;
+	int quantity =0;
+	char currency[3] ="€";
+	float total =0;
 
-	printf(" %f",gpa);
-
+	printf("Currency is: %s",currency);
 
 	return 0;
-}
+} 
+
+// to change variable permanently, you have to change the value in the adress with &
+// no need to do it in fgets, because string is already a pointer (to its first character)
+
+// + € is a string[3], unlike $, so i need to use a string if i want it in a variable
