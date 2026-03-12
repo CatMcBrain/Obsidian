f3c929d3744250057ebe898a462ccfd07c04e50d
Cat_McBrain <ajersa05@gmail.com>
2026-03-12 20:36:57 +0100

    vault backup: 12-03-2026 20:36:57

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main()
 {
-	char x[20]="test";
+	float in = 0;
+	float out = 0;
+	int choice = 0;
 
-	printf("Total: %d String: %d", sizeof(x), strlen(x));
+	printf("Type 1 for kg->pnd or 2 for pnd->kg: ");
+	scanf(" %n", &choice);
 
-	return 0;
+	if(choice==1){
+		printf("How many kg?: ");
+		scanf(" %f", &in);
+		out=in*2.205;
+		printf("The result is: %f pnd", out);
+	}
+	else if(choice==0){
+		printf("How many pnd?: ");
+		scanf(" %f", &in);
+		out=in/2.205;
+		printf("The result is: %f kg", out);
+	}
+	else{
+		printf("Invalid choice.");
+	}
 } 
