6ff5e23a7c918830b065c43f66c0dfcd614337f2
Cat_McBrain <ajersa05@gmail.com>
2026-03-12 19:32:21 +0100

    vault backup: 12-03-2026 19:32:21

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 
-	int x=1;
-	int * px = &x;
-	int vx = *&x;
+	char noun[50] = "";
+	char verb[50]= "";
+	char adjective1[50]= "";
+	char adjective2[50]= "";
+	char adjective3[50]= "";
 
-	printf("x=%d , px=%d , vx= %d ", x, px, vx);
+	printf("Tell me an adjective: ");
+	fgets(adjective1, sizeof(adjective1), stdin);
+	printf("\n Tell me an noun: ");
+	fgets(noun, sizeof(noun), stdin);
+	printf("\n Tell me an adjective: ");
+	fgets(adjective2, sizeof(adjective2), stdin);
+	printf("\n Tell me an verb (-ing): ");
+	fgets(verb, sizeof(verb), stdin);
+	printf("\n Tell me an adjective: ");
+	fgets(adjective3, sizeof(adjective3), stdin);
+
+	printf(" %s\n", noun);
+	printf(" %s\n", adjective1);
+	printf(" %s\n", adjective2);
+	printf(" %s\n", adjective3);
+	printf(" %s\n", verb);
+
 
 	return 0;
 } 
-
-// to change variable permanently, you have to change the value in the adress with &
-// no need to do it in fgets, because string is already a pointer (to its first character)
-
-// + € is a string[3], unlike $, so i need to use a string if i want it in a variable
