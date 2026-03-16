5b89ba90f2ea7e3bbc17fccf3221ac6ae7313f7e
Cat_McBrain <ajersa05@gmail.com>
2026-03-16 19:53:00 +0100

    vault backup: 16-03-2026 19:52:59

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 
 int main()
 {
-	int n = 0;
 
-	for(int i=10; i=0; i--){
-		n += n ;
+	for(int i=0; i<10; i++){
 		printf("%d\n", i);
 	}
 
