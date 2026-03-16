00140a3bba96a2c0a27a4d94e0057f33bd98da9c
Cat_McBrain <ajersa05@gmail.com>
2026-03-16 21:25:59 +0100

    vault backup: 16-03-2026 21:25:59

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -4,14 +4,14 @@ int main()
 {
 	int rows=0;
 	int cols =0;
-	char ch='?';
+	char ch='\0';
 
 	printf("How many rows?: ");
-	scanf("%d",&rows);
+	scanf(" %d",&rows);
 	printf("How many columns?: ");
-	scanf("%d",&cols);
+	scanf(" %d",&cols);
 	printf("Character?: ");
-	scanf("%c",&ch);
+	scanf(" %c",&ch);
 
 
 	for(int i=rows;i>0;i--){
