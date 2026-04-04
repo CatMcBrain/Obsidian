909234322fa9190722524a90bb8fe44372c5aa51
Cat_McBrain <ajersa05@gmail.com>
2026-04-04 21:05:08 +0200

    vault backup: 04-04-2026 21:05:07

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -2,15 +2,21 @@
 
 int main(){
 	
-	FILE *pFile = fopen("file.txt", "r");
+float res=0.0;
+int num =0;
 
-char buffer[1024]={0};
-
-	while(fgets(buffer, 1024,pFile) != NULL){
-	printf("%s", buffer);
+	for(int i=1;i<=20;i++){
+		num=0;
+		for(int j=1;j<=i;j++){
+			res=i%j;
+			if(res==0.0){
+				num++;
+			
+		}
+		if(num==2){
+			printf("%d",i);
+		}
 	}
-
-fclose(pFile);
 
 	return 0;
 }
