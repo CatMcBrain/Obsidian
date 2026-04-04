42f9adf4632367d8dff4ba9ac218791afe35cdf1
Cat_McBrain <ajersa05@gmail.com>
2026-04-04 19:26:33 +0200

    vault backup: 04-04-2026 19:26:33

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,23 +1,11 @@
 #include<stdio.h>
-#include<string.h>
-
-struct test{
-	char name[10];
-	int num;
-};
 
 int main(){
 	
-	struct test foo1 = {"foo1",1};
-	struct test foo2 = {"foo2",2};
+	int foo = 1;
+	int add = &foo;
 
-	struct test foos[]= {foo1, foo2};
-	int len=sizeof(foos)/sizeof(foos[0]);
-
-	for(int i=0;i<len;i++){
-		printf("%s\n",foos[i].name);
-		printf("%d\n",foos[i].num);
-	};
+	printf("%p", add);
 
 	return 0;
 }
