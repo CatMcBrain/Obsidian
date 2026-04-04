880b71840b736fd01b63a92684c3f3bce7b73d26
Cat_McBrain <ajersa05@gmail.com>
2026-04-04 21:07:15 +0200

    vault backup: 04-04-2026 21:07:14

--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+// argv[1]= n-ésimo
 
-int main(){
-	
+int main(int argc, char* argv[]){
+
 float res=0.0;
 int num =0;
 
-	for(int i=1;i<=20;i++){
+	for(int i=1;i<=argv[1];i++){
 		num=0;
 		for(int j=1;j<=i;j++){
 			res=i%j;
