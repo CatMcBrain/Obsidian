f9d3b533c5b214a7378c1ef27dbf448ba36ddf53
Cat_McBrain <ajersa05@gmail.com>
2026-04-07 18:39:52 +0200

    vault backup: 07-04-2026 18:39:52

---
+++ Universidad/COMPUTACIÓN/test/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+
+}
+for(int i=0;i<len;i++){
+	printf("#%d de matriz b: ",i+1);
--- Universidad/COMPUTACIÓN/test/test.c
+++ Universidad/COMPUTACIÓN/test/test.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-int main(int argc, char* argv[]){
+void vprint (float a[]){
+	int len = sizeof(a)/sizeof(a[0]);
+	for(int i=0;i<=len;i++){
+		printf("%f ", a[i]);
+	}
+	printf("\n");
+}
 
-int num = atoi(argv[1]);
+int main(){
 
-printf("%d",num);
+float a[3]={0};
+float b[3]={0};
+int len = sizeof(a)/sizeof(a[0]);
 
-	return 0;
+for(int i=0;i<len;i++){
+	printf("#%d de matriz a: ",i+1);
+	scanf(" %f", &a[i]);
+}
+for(int i=0;i<len;i++){
+	printf("#%d de matriz b: ",i+1);
+	scanf(" %f", &b[i]);
+}
+
+vprint(a);
+vprint(b);
+
+return 0;
 }
