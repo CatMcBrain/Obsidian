ca1545bd0d5ae1af37c9590a00c80d9950f53006
Cat_McBrain <ajersa05@gmail.com>
2026-04-11 20:56:44 +0200

    vault backup: 11-04-2026 20:56:44

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
@@ -6,18 +6,43 @@ const float Dv = 10;
 const float a = 0.1;
 const float b = 0.9;
 const float dt = 0.01; // segundos
-const float T = 200 ; // segundos
+const int T = 200 ; // segundos
 
-void laplace(float (*m)[L][L], int n){
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			(*m)[i][j] = m[i+1][j]+m[i-1][j]+m[i][j+1]+m[i][j-1]-4*m[i][j];
+void laplace(float (*m)[L][L]){ // 3) Laplaciano
+	for(int i=0; i<L; i++){
+		for(int j=0; j<L; j++){
+			(*m)[i][j] = (*m)[i+1][j]+(*m)[i-1][j]+(*m)[i][j+1]+(*m)[i][j-1]-4*(*m)[i][j];
 		}
 	}
 }
+
+float timestep(float (*m)[L][L], char mode){ // 4) Actualización temporal
+	float res[L][L] = {};
+	int n = 0;
+	float var = 0; // para englobar el caso de u y el de v en una sóla función
+
+	for(int t=0; t<=T; t++){ // paso del tiempo
+		for(int i=0; i<L; i++){
+			for(int j=0; j<L; j++){
+				if (t != 0){ // condición periódica
+					n = t-1;
+				}else{
+					n = T;
+				}
+				(*m)[i][j] = 0; // ecuación de actualización temporal
+			}
+		}
+	}
+}
+
 int main(){
 
-	float red[L][L] = {};
+	float redu[L][L] = {};
+	float redv[L][L] = {};
+
+	// me falta saber inicializar la red
+	// luego meter perturbaciones
+	
 
 
 	return 0;
