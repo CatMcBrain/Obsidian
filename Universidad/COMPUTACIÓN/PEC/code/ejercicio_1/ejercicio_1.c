25fc3a5037e7ab06f945fd8c09b72ae364f53350
Cat_McBrain <ajersa05@gmail.com>
2026-04-11 18:47:19 +0200

    vault backup: 11-04-2026 18:47:19

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
@@ -9,8 +9,8 @@ const float dt = 0.01; // segundos
 const float T = 200 ; // segundos
 
 void laplace(float *m[][], int n){
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=n; j++){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
 			*m[i][j] = m[i+1][j]+m[i-1][j]+m[i][j+1]+m[i][j-1]-4*m[i][j];
 		}
 	}
