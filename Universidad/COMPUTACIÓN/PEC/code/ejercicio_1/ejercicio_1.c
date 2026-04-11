24298b940e6dbf975255bc29b913c89e22a56599
Cat_McBrain <ajersa05@gmail.com>
2026-04-11 11:00:35 +0200

    vault backup: 11-04-2026 11:00:35

---
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+const int L = 100;
+const float Du = 1;
+const float Dv = 10;
+const float a = 0.1;
+const float b = 0.9;
+const float dt = 0.01; // segundos
+const float T = 200 ; // segundos
+
+float laplace(px){
+
+
+}
+int main(){
+
+	float red[L][L] = {};
+
+
+	return 0;
+}
