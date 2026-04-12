fa6288c9429825240afad8b347d60c1f5fe68e70
Cat_McBrain <ajersa05@gmail.com>
2026-04-12 13:52:06 +0200

    vault backup: 12-04-2026 13:52:06

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
 const int L = 100;
 const float Du = 1;
@@ -8,7 +11,10 @@ const float b = 0.9;
 const float dt = 0.01; // segundos
 const int T = 200 ; // segundos
 
-void laplace(float (*m)[L][L]){ // 3) Laplaciano
+const int archNum = 10; // número de archivos deseados
+const int archStep = 200; // paso temporal de cada archivo nuevo
+
+void mlaplace(float (*m)[L][L]){ // 3) Laplaciano
 	for(int i=0; i<L; i++){
 		for(int j=0; j<L; j++){
 			(*m)[i][j] = (*m)[i+1][j]+(*m)[i-1][j]+(*m)[i][j+1]+(*m)[i][j-1]-4*(*m)[i][j];
@@ -16,33 +22,74 @@ void laplace(float (*m)[L][L]){ // 3) Laplaciano
 	}
 }
 
-float timestep(float (*m)[L][L], char mode){ // 4) Actualización temporal
-	float res[L][L] = {};
-	int n = 0;
-	float var = 0; // para englobar el caso de u y el de v en una sóla función
+float laplace(float m[L][L], int a, int b){
+	return  m[a+1][b]+m[a-1][b]+m[a][b+1]+m[a][b-1]-4*m[a][b] ;
+}
 
-	for(int t=0; t<=T; t++){ // paso del tiempo
+void timestep(float u[L][L], float v[L][L]){ // 4) Actualización temporal
+	// En t = 0, será el estado inicial de u y v
+	float buffer_u[L][L] = {0}; // guardar el valor de u para usarlo en v
+	
+	int currNum = 0; // número de archivo
+	int currStep = 1; // cuento t=0 como primer paso
+
+	for(int t=1; t<=T/dt; t++){ // paso del tiempo desde t = 1
 		for(int i=0; i<L; i++){
 			for(int j=0; j<L; j++){
-				if (t != 0){ // condición periódica
-					n = t-1;
-				}else{
-					n = T;
-				}
-				(*m)[i][j] = 0; // ecuación de actualización temporal
+				fill(&buffer_u,u);
+				// ecuación de actualización temporal desplazada n-1
+				u[i][j] = u[i][j]+dt*(Du*laplace(u, i, j)+a-(u[i][j] * u[i][j]) * v[i][j]);
+				v[i][j] = v[i][j]+dt*(Dv*laplace(v, i, j)+b-(buffer_u[i][j] * buffer_u[i][j]) * v[i][j]);
 			}
 		}
+		currStep++;
+		if(currStep == archStep){
+			// escribir el estado actual de u en un archivo
+			//<>
+			currNum++;
+		}
+		if(currNum == archNum){
+			break;
+		}
+		
 	}
 }
 
+void fill(float (*ini)[L][L], float fin[L][L]){
+	for(int i=0; i< L; i++){
+		for(int j=0; j<L; j++){
+			(*ini)[i][j] = fin[i][j];
+		}
+	}
+} 
+
+// -----------------------------------------------------
+
 int main(){
+
+	srand(time(NULL)); // para números aleatorios
 
 	float redu[L][L] = {};
 	float redv[L][L] = {};
-
-	// me falta saber inicializar la red
-	// luego meter perturbaciones
 	
+
+// inicializo las redes con perturbaciones
+for(int i=0; i<L; i++){
+	for(int j=0; j<L; j++){
+		// defino perturbaciones normalizando rand max y convertir el rango a [-1,1]
+		float pert = 0.01*(2.0*rand() / RAND_MAX - 1.0);
+		redu[i][j] = a+b + pert;
+		redv[i][j] = b/((a+b)*(a+b)) + pert;
+	}
+}
+
+// laplaciano
+
+mlaplace(&redu);
+mlaplace(&redv);
+
+// actualización temporal
+
 
 
 	return 0;
