e249d13d8a8d68f3c82dbbb4d6493419ced755c5
Cat_McBrain <ajersa05@gmail.com>
2026-04-14 19:00:58 +0200

    vault backup: 14-04-2026 19:00:58

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_1/ejercicio_1.c
@@ -3,49 +3,51 @@
 #include<math.h>
 #include<time.h>
 
+const double Pi = 3.14159265358979323846;
 const int L = 100;
-const float Du = 1;
-const float Dv = 10;
-const float a = 0.1;
-const float b = 0.9;
-const float dt = 0.01; // segundos
+const double Du = 1;
+const double Dv = 10;
+const double a = 0.1;
+const double b = 0.9;
+const double dt = 0.01; // segundos
 const int T = 200 ; // segundos
 
 const int archNum = 10; // número de archivos deseados
-const int archStep = 200; // paso temporal de cada archivo nuevo
+const int archStep = 2000; // paso temporal de cada archivo nuevo
+const double sigma = 1e-3; // para obtener valores de ruido del orden pedido
 
-void mlaplace(float (*m)[L][L]){ // 3) Laplaciano
-	for(int i=0; i<L; i++){
-		for(int j=0; j<L; j++){
-			(*m)[i][j] = (*m)[i+1][j]+(*m)[i-1][j]+(*m)[i][j+1]+(*m)[i][j-1]-4*(*m)[i][j];
-		}
-	}
+double gauss(double sig){
+	double u1 = (double)rand() / RAND_MAX;
+	double u2 = (double)rand() / RAND_MAX;
+	// transformada de Box-Muller
+	return sig * sqrt(-2.0 * log(u1)) * cos(2.0 * Pi * u2);
 }
 
-float laplace(float m[L][L], int a, int b){
+// Laplaciano para usar en la función del paso temporal
+double laplace(double m[L][L], int a, int b){
 	return  m[a+1][b]+m[a-1][b]+m[a][b+1]+m[a][b-1]-4*m[a][b] ;
 }
 
-void timestep(float u[L][L], float v[L][L]){ // 4) Actualización temporal
+void timestep(double u[L][L], double v[L][L]){ // 4) Actualización temporal
 	// En t = 0, será el estado inicial de u y v
-	float buffer_u[L][L] = {0}; // guardar el valor de u para usarlo en v
+	double buffer_u[L][L] = {0}; // guardar el valor de u para usarlo en v
 	
 	int currNum = 0; // número de archivo
-	int currStep = 1; // cuento t=0 como primer paso
+	int currStep = 0; 
 
 	for(int t=1; t<=T/dt; t++){ // paso del tiempo desde t = 1
+		fill(&buffer_u,u);
 		for(int i=0; i<L; i++){
 			for(int j=0; j<L; j++){
-				fill(&buffer_u,u);
 				// ecuación de actualización temporal desplazada n-1
-				u[i][j] = u[i][j]+dt*(Du*laplace(u, i, j)+a-(u[i][j] * u[i][j]) * v[i][j]);
+				u[i][j] = u[i][j]+dt*(Du*laplace(u, i, j)+a-u[i][j]+(u[i][j] * u[i][j]) * v[i][j]);
 				v[i][j] = v[i][j]+dt*(Dv*laplace(v, i, j)+b-(buffer_u[i][j] * buffer_u[i][j]) * v[i][j]);
 			}
 		}
 		currStep++;
 		if(currStep == archStep){
 			// escribir el estado actual de u en un archivo
-			//<>
+			filesave(u, currNum);
 			currNum++;
 		}
 		if(currNum == archNum){
@@ -55,7 +57,7 @@ void timestep(float u[L][L], float v[L][L]){ // 4) Actualización temporal
 	}
 }
 
-void fill(float (*ini)[L][L], float fin[L][L]){
+void fill(double (*ini)[L][L], double fin[L][L]){
 	for(int i=0; i< L; i++){
 		for(int j=0; j<L; j++){
 			(*ini)[i][j] = fin[i][j];
@@ -63,7 +65,7 @@ void fill(float (*ini)[L][L], float fin[L][L]){
 	}
 } 
 
-void filesave(float m[L][L], int count) {
+void filesave(double m[L][L], int count) {
     char nombre[50] = {'\0'};
     sprintf(nombre, "u_%d.dat", count); // nombre dinámico
 
@@ -85,28 +87,24 @@ int main(){
 
 	srand(time(NULL)); // para números aleatorios
 
-	float redu[L][L] = {};
-	float redv[L][L] = {};
+	double redu[L][L] = {};
+	double redv[L][L] = {};
 	
 
-// inicializo las redes con perturbaciones
+// inicializo las redes con perturbaciones (ruido Gaussiano)
 for(int i=0; i<L; i++){
 	for(int j=0; j<L; j++){
-		// defino perturbaciones normalizando rand max y convertir el rango a [-1,1]
-		float pert = 0.01*(2.0*rand() / RAND_MAX - 1.0);
+		double pert = gauss(sigma);
 		redu[i][j] = a+b + pert;
 		redv[i][j] = b/((a+b)*(a+b)) + pert;
 	}
 }
 
-// laplaciano
-
-mlaplace(&redu);
-mlaplace(&redv);
-
 // actualización temporal
 
+timestep(redu, redv);
 
+// <REVISAR contorno>
 
 	return 0;
 }
