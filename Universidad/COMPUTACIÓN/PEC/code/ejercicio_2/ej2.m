261181f0720a8359386245b34739889e0edf50fc
Cat_McBrain <ajersa05@gmail.com>
2026-04-21 20:52:12 +0200

    vault backup: 21-04-2026 20:52:11

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
@@ -10,10 +10,12 @@ end
 
 % Valor máximo y mínimo de color se halla automáticamente en el colormap <?>, lo podemos observar con colorbar
 exit = 1;
-while exit==1 
+while exit==1
+    for n=1:10
     for i=1:10
         surf(1:100,1:100,redu{i});
-        pause(3);
+        pause();
     end
-    % exit = input("¿Deseas salir? ()");
+    end
+    exit = input("¿Deseas salir? (0/1)");
 end
