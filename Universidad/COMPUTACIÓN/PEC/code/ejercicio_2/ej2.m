655dc3e1545503c4dbb8a496f8b2323c9c45a3f3
Cat_McBrain <ajersa05@gmail.com>
2026-04-21 21:25:38 +0200

    vault backup: 21-04-2026 21:25:37

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
@@ -16,15 +16,13 @@ for i=0:9
    umax = max([umax, vdata]);
 end
 
-% Valor máximo y mínimo de color se halla automáticamente en el colormap <?>, lo podemos observar con colorbar
-exit = 1;
-while exit==1
-    for n=1:10
+
+while 1==1
     for i=1:10
         surf(1:100,1:100,redu{i});
+        clim(umin,umax);
+        colorbar;
         view(2); % vista desde arriba en 2D
         drawnow;
     end
-    end
-    % exit = input("¿Deseas salir? (0/1)");
 end
