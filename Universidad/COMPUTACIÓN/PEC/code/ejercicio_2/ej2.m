098f38335181b5c30fa9b1ce0dfec561553134ad
Cat_McBrain <ajersa05@gmail.com>
2026-04-21 12:53:25 +0200

    vault backup: 21-04-2026 12:53:25

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
@@ -1,6 +1,8 @@
 % Importar datos del ejercicio 1
 time = readmatrix("../ejercicio_1/time.dat");
 amp = readmatrix("../ejercicio_1/amplitud.dat");
+% Bucle para leer los archivos de la red u
 for i=0:9
+    filename = sprintf();
     u
 end
