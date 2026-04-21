df7644b91b1f7c0ff60e617b88fec330a26d8b2f
Cat_McBrain <ajersa05@gmail.com>
2026-04-21 16:19:52 +0200

    vault backup: 21-04-2026 16:19:52

--- Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
+++ Universidad/COMPUTACIÓN/PEC/code/ejercicio_2/ej2.m
@@ -3,9 +3,11 @@ time = readmatrix("../ejercicio_1/time.dat");
 amp = readmatrix("../ejercicio_1/amplitud.dat");
 % Bucle para leer los archivos de la red u
 redu = cell(0,9);
-{%
+%{
 for i=0:9
     filename = sprintf("../ejercicio_1/u_%d.dat",i);
     redu{i} = readmatrix(filename);
 end
 %}
+
+redu{0} = readmatrix("../ejercicio_1/u_0.dat");
